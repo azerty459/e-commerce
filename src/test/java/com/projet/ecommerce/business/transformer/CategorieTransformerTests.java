e978dd92319432579d655cdb39c43c9bc86f3689
pawaxin <lleger@nextoo.fr>
2018-04-11 10:32:59 +0200

    abandon temporaire du test des transformer

---
+++ src/test/java/com/projet/ecommerce/business/transformer/CategorieTransformerTests.java
@@ -0,0 +1,38 @@
+// A finir
+
+/*package com.projet.ecommerce.business.transformer;
+
+import com.projet.ecommerce.business.dto.CaracteristiqueDTO;
+import com.projet.ecommerce.business.dto.CategorieDTO;
+import com.projet.ecommerce.business.dto.transformer.CategorieTransformer;
+import com.projet.ecommerce.persistance.entity.Caracteristique;
+import com.projet.ecommerce.persistance.entity.Categorie;
+import com.projet.ecommerce.persistance.entity.TypeCaracteristique;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class CategorieTransformerTests {
+
+    private Categorie categorie;
+    private CategorieDTO categorieDTO1;
+
+    @Before
+    public void setUp() {
+        categorie = new Categorie();
+        categorie.setNomCategorie("test");
+
+        categorieDTO1 = new CategorieDTO();
+        categorieDTO1.
+    }
+
+    @Test
+    public void singleDtoToEntity(){
+        Categorie cat = CategorieTransformer.dtoToEntity()
+    }
+}*/
