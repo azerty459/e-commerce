df347b087f50ac61135c7ed8a48faae426123be7
pawaxin <lleger@nextoo.fr>
2018-04-12 15:47:41 +0200

    ajout de la documentation de Query

--- src/main/java/com/projet/ecommerce/entrypoint/graphQL/Query.java
+++ src/main/java/com/projet/ecommerce/entrypoint/graphQL/Query.java
@@ -11,33 +11,59 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 /**
- * Class permettant de définir les méthodes à utiliser pour les Query de graphQL.
+ * Classe implémentant les méthodes de requête de graphQL.
  */
 @Component
 public class Query implements GraphQLQueryResolver {
 
+    /**
+     *
+     */
     @Autowired
     private ICategorieBusiness categorieBusiness;
 
     @Autowired
     private IProduitBusiness produitBusiness;
 
+    /**
+     * Obtenir tous les produits présents en base
+     * @return la liste des produits présents en base
+     */
     public List<ProduitDTO> getAllProduit() {
         return produitBusiness.getAll();
     }
 
+    /**
+     * Obtenir un produit en fonction de sa référence
+     * @param referenceProduit la référence du produit voulu
+     * @return le produit recherché
+     */
     public ProduitDTO getProduitByRef(String referenceProduit) {
         return produitBusiness.getByRef(referenceProduit);
     }
 
+    /**
+     * Obtenir les produits appartenant à une catégorie
+     * @param nomCategorie la catégorie voulue
+     * @return la liste des produits appartenants à la catégorie recherchée
+     */
     public List<ProduitDTO> getProduitByCategorie(String nomCategorie){
         return produitBusiness.getByCategorie(nomCategorie);
     }
 
+    /**
+     * Obtenir toute les catégories présentes en base
+     * @return la liste des catégories présentes en base
+     */
     public List<CategorieDTO> getAllCategorie() {
         return categorieBusiness.getAll();
     }
 
+    /**
+     * Obtenir une catégorie en fonction de son nom
+     * @param nomCategorie le nom de la catégorie recherchée
+     * @return la catégorie trouvée
+     */
     public CategorieDTO getCategorieByNom(String nomCategorie) {
         return categorieBusiness.getByNom(nomCategorie);
     }
