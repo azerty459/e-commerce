85332ab10f60f296d09a261dfe1bc7a4c0c11273
Rémy Halipré <rhalipre@nextoo.fr>
2018-04-12 18:34:26 +0200

    Modif Tests

--- src/test/java/com/projet/ecommerce/graphql/TestMutation.java
+++ src/test/java/com/projet/ecommerce/graphql/TestMutation.java
@@ -15,6 +15,10 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
 import static org.hamcrest.CoreMatchers.instanceOf;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -37,9 +41,7 @@ public class TestMutation {
     @Test
     public void addProduit(){
         Mockito.when(produitBusiness.add(Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.anyFloat())).thenReturn(new ProduitDTO());
-        ProduitDTO retour = mutation.addProduit("A4224", "Livre", "Un livre", 4.5f);
-
-        // lorsque l'ajout a fonctionné, on reçoit le produit ajouté
+        ProduitDTO retour = mutation.addProduit("Test", "Test", "Test" , 4.7f);
         Assert.assertNotNull(retour);
         Assert.assertThat(retour, instanceOf(ProduitDTO.class));
     }
@@ -63,15 +65,21 @@ public class TestMutation {
         Assert.assertTrue((mutation.deleteProduit("A4224")));
     }
 
-    /*@Test
+    @Test
     public void addCategorie(){
-        Mockito.when(categorieBusiness.addCategorie(Mockito.anyString(), Mockito.anyString())).thenReturn(new CategorieDTO());
-        ProduitDTO retour = mutation.addProduit("A4224", "Livre", "Un livre", 4.5f);
+        Mockito.when(categorieBusiness.add(Mockito.anyString(), Mockito.anyString())).thenReturn(null);
+        CategorieDTO retour = mutation.addCategorie("Test", "Test");
+        Assert.assertNull(retour);
 
-        // lorsque l'ajout a fonctionné, on reçoit le produit ajouté
-        Assert.assertNotNull(retour);
-        Assert.assertThat(retour, instanceOf(ProduitDTO.class));
-    }*/
+        CategorieDTO categorieDTO = new CategorieDTO();
+        categorieDTO.setNom("Transport");
+        categorieDTO.setSousCategories(new ArrayList<>());
+
+        Mockito.when(categorieBusiness.add(Mockito.anyString(), Mockito.anyString())).thenReturn(categorieDTO);
+        CategorieDTO retour2 = mutation.addCategorie("Test", "Test");
+        Assert.assertNotNull(retour2);
+        Assert.assertThat(retour2, instanceOf(CategorieDTO.class));
+    }
 
     @Test
     public void deleteCategorie(){
