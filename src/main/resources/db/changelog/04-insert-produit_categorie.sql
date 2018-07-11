d0c1fd43b12eab05b3ad50856340b516d73dbbe1
rantoine <aruffino@nextoo.fr>
2018-07-11 11:44:48 +0200

    correction script ini

--- src/main/resources/db/changelog/04-insert-produit_categorie.sql
+++ src/main/resources/db/changelog/04-insert-produit_categorie.sql
@@ -1,14 +1,14 @@
 --liquibase formatted sql
 --changeset Rémy Halipré:04
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A05A01', 25);
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A05A02', 25);
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A05A03', 25);
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A05A04', 25);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A05A01', 1);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A05A02', 2);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A05A03', 1);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A05A04', 3);
 
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A06A01', 30);
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A06A02', 30);
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A06A03', 30);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A06A01', 1);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A06A02', 2);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A06A03', 2);
 
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A07A01', 31);
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A07A02', 31);
-INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A07A03', 31);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A07A01', 2);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A07A02', 2);
+INSERT INTO produit_categorie (reference_produit, id_categorie) VALUES ('A07A03', 2);
