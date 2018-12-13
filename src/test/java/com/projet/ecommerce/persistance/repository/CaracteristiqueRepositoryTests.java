8c4d55a4269da9ac9fb3d4f4f1be31729b62927b
tkhadir <tkhadir@nextoo.fr>
2018-12-13 11:58:19 +0100

    sprint 1 # tkr # mvn clean install ok

--- src/test/java/com/projet/ecommerce/persistance/repository/CaracteristiqueRepositoryTests.java
+++ src/test/java/com/projet/ecommerce/persistance/repository/CaracteristiqueRepositoryTests.java
@@ -7,13 +7,13 @@ import com.projet.ecommerce.persistance.entity.TypeCaracteristique;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,11 +31,45 @@ public class CaracteristiqueRepositoryTests {
     private ProduitRepository produitRepository;
 
     private static final String REFERENCE_PRODUIT = "A05A01";
-    private static final Logger logger = LoggerFactory.getLogger(CaracteristiqueRepositoryTests.class);
+    private static Produit PRODUIT;
+    private static Caracteristique CARACTERISTIQUE;
+    private static TypeCaracteristique TYPECARACTERISTIQUE;
+
+    static {
+        //Permet d'écraser la config application.properties par application-test.properties
+        System.setProperty("spring.config.location", "classpath:application-test.properties");
+        PRODUIT = new Produit();
+        PRODUIT.setReferenceProduit("A05A01");
+        PRODUIT.setPrixHT(8.7f);
+        PRODUIT.setDescription("joli produit");
+        PRODUIT.setCaracteristiques(new ArrayList<>());
+
+        CARACTERISTIQUE = new Caracteristique();
+        CARACTERISTIQUE.setProduit(PRODUIT);
+        CARACTERISTIQUE.setValeur("test n#1");
+
+        TYPECARACTERISTIQUE = new TypeCaracteristique();
+        TYPECARACTERISTIQUE.setType("test type");
+        CARACTERISTIQUE.setType(TYPECARACTERISTIQUE);
+    }
+
+    @Test
+    public void updateCaracteristique() {
+        produitRepository.save(PRODUIT);
+        typeCaracteristiqueRepository.save(TYPECARACTERISTIQUE);
+        Caracteristique saved = caracteristiqueRepository.save(CARACTERISTIQUE);
+        Caracteristique c = caracteristiqueRepository.findById(saved.getIdCaracteristique()).get();
+        Assert.assertNotNull(c);
+        c.setValeur("test test");
+        Assert.assertNotNull(caracteristiqueRepository.save(c));
+        Assert.assertEquals(c.getType().getIdTypeCaracteristique(), typeCaracteristiqueRepository.findById(c.getType().getIdTypeCaracteristique()).get().getIdTypeCaracteristique());
+    }
 
     @Test
     public void insertCaracteristique() {
+        produitRepository.save(PRODUIT);
         Produit p = produitRepository.findByReferenceProduit(REFERENCE_PRODUIT);
+        Assert.assertNotNull(p);
         TypeCaracteristique t= new TypeCaracteristique();
         t.setType("test type");
         Caracteristique c= new Caracteristique();
@@ -44,18 +78,13 @@ public class CaracteristiqueRepositoryTests {
         Assert.assertNotNull(savedT);
         c.setType(savedT);
         c.setProduit(p);
-        Assert.assertNotNull(caracteristiqueRepository.save(c));
-        Assert.assertTrue(p.getCaracteristiques().contains(c));
+        p.getCaracteristiques().add(c);
+        c = caracteristiqueRepository.save(c);
+        Assert.assertNotNull(c);
+        Assert.assertTrue(produitRepository.findByReferenceProduit(REFERENCE_PRODUIT).getCaracteristiques().contains(c));
     }
 
-    @Test
-    public void updateCaracteristique() {
-        Caracteristique c = caracteristiqueRepository.findById(1).get();
-        Assert.assertNotNull(c);
-        c.setValeur("test test");
-        Assert.assertNotNull(caracteristiqueRepository.save(c));
-        Assert.assertEquals(c.getType().getIdTypeCaracteristique(), typeCaracteristiqueRepository.findById(c.getType().getIdTypeCaracteristique()).get().getIdTypeCaracteristique());
-    }
+
 
 
 }
