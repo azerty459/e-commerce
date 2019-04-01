6b1908045666c9671a398c616ca9f885ffb27256
Olivier DELBOUVE <odelbouve@nextoo.fr>
2019-04-01 18:46:33 +0200

    Correction conf pour l'appli

--- src/main/java/com/projet/ecommerce/business/impl/PhotoBusiness.java
+++ src/main/java/com/projet/ecommerce/business/impl/PhotoBusiness.java
@@ -11,6 +11,7 @@ import com.projet.ecommerce.persistance.repository.PhotoRepository;
 import com.projet.ecommerce.persistance.repository.ProduitRepository;
 import com.projet.ecommerce.utilitaire.ImageUtilitaire;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
@@ -31,17 +32,18 @@ import java.util.Optional;
 public class PhotoBusiness implements IPhotoBusiness {
 
     private ImageUtilitaire imageUtilitaire = new ImageUtilitaire();
+
     @Autowired
     private PhotoRepository photoRepository;
 
     @Autowired
     private ProduitRepository produitRepository;
 
-    //TODO aller chercher la variable dans le fichier properties
     /**
      * Le répertoire de base pour les images.
      */
-    private static final String repertoireImg = "src/main/resources/img/";
+    @Value("repertoire.img")
+    private String repertoireImg;
 
     /**
      * Methode permettant l'upload d'un fichier
