ac81ba9c794c5c43fc1b01ff425c2a29c37c264f
NicolasTheRookie <n.kowalski@comptoirducode.com>
2019-04-04 16:03:13 +0200

    Ajout des DTO et Transformer pour les caracteristiques

---
+++ src/main/java/com/projet/ecommerce/business/dto/CaracteristiqueDTO.java
@@ -0,0 +1,30 @@
+package com.projet.ecommerce.business.dto;
+
+public class CaracteristiqueDTO {
+	/**
+	 * l'id de la caractéristique
+	 */
+	private int idCaracteristique;
+	/**
+	 * la valeur de la caractéristique
+	 */
+	private String valeur;
+	
+	public int getIdCaracteristique() {
+		return idCaracteristique;
+	}
+	
+	public void setIdCaracteristique(int idCaracteristique) {
+		this.idCaracteristique = idCaracteristique;
+	}
+	
+	public String getValeur() {
+		return valeur;
+	}
+	
+	public void setValeur(String valeur) {
+		this.valeur = valeur;
+	}
+	
+	
+}
---
+++ src/main/java/com/projet/ecommerce/business/dto/transformer/TypeCaracteristiqueTransformer.java
@@ -0,0 +1,47 @@
+package com.projet.ecommerce.business.dto.transformer;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.projet.ecommerce.business.dto.TypeCaracteristiqueDTO;
+import com.projet.ecommerce.persistance.entity.TypeCaracteristique;
+
+public class TypeCaracteristiqueTransformer {
+	
+	public TypeCaracteristiqueTransformer() {
+		super();
+	}
+
+	private static TypeCaracteristique dtoToEntity(TypeCaracteristiqueDTO typeCaracteristiqueDTO) {
+		TypeCaracteristique typeCaracteristique = new TypeCaracteristique();
+		typeCaracteristique.setIdTypeCaracteristique(typeCaracteristiqueDTO.getIdTypeCaracteristique());
+		typeCaracteristique.setTypeCaracteristique(typeCaracteristiqueDTO.getTypeCaracteristique());
+		return typeCaracteristique;
+	}
+	
+	public static Collection<TypeCaracteristique> dtoToEntity(Collection<TypeCaracteristiqueDTO> typeCaracteristiqueDTOCollection){
+		List<TypeCaracteristique> listeTypeCaracteristique = new ArrayList<TypeCaracteristique>();
+		for(TypeCaracteristiqueDTO typeCaracteristiqueDTO : typeCaracteristiqueDTOCollection) {
+			listeTypeCaracteristique.add(dtoToEntity(typeCaracteristiqueDTO));
+		}
+		
+		return listeTypeCaracteristique;
+	}
+	
+	private static TypeCaracteristiqueDTO entityToDto(TypeCaracteristique typeCaracteristique) {
+		TypeCaracteristiqueDTO typeCaracteristiqueDTO = new TypeCaracteristiqueDTO();
+		typeCaracteristiqueDTO.setIdTypeCaracteristique(typeCaracteristique.getIdTypeCaracteristique());
+		typeCaracteristiqueDTO.setTypeCaracteristique(typeCaracteristique.getTypeCaracteristique());
+		return typeCaracteristiqueDTO;
+	}
+	
+	public static Collection<TypeCaracteristiqueDTO> entityToDTO(Collection<TypeCaracteristique> typeCaracteristiqueCollection){
+		List<TypeCaracteristiqueDTO> listeTypeCaracteristiqueDTO = new ArrayList<TypeCaracteristiqueDTO>();
+		for(TypeCaracteristique typeCaracteristique : typeCaracteristiqueCollection) {
+			listeTypeCaracteristiqueDTO.add(entityToDto(typeCaracteristique));
+		}
+		
+		return listeTypeCaracteristiqueDTO;
+	}
+}
