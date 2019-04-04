a2838bfbb8f156610a0f8d0c5dcd1c57d55e98ff
Abbas Attaf <abbas.attaf@gmail.com>
2019-04-04 17:50:16 +0200

    correction script SQL

--- src/main/resources/db/changelog/10-insert-caracteristique.sql
+++ src/main/resources/db/changelog/10-insert-caracteristique.sql
@@ -9,8 +9,11 @@ CREATE TABLE type_caracteristique (
 CREATE TABLE caracteristique (
 
     id_caracteristique SERIAL,
+    id_type_caracteristique SERIAL,
+    valeur VARCHAR(255),
+    reference_produit VARCHAR(255),
     PRIMARY KEY (id_caracteristique),
-    CONSTRAINT FK_idCategorie FOREIGN KEY (id_type_caracteristique)
+    CONSTRAINT FK_idType_caracteristique FOREIGN KEY (id_type_caracteristique)
     REFERENCES type_caracteristique(id_type_caracteristique),
     CONSTRAINT FK_idProduit FOREIGN KEY (reference_produit)
     REFERENCES produit(reference_produit)
