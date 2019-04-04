3ca365b91048abcc7133f93d1a62155aaeae7839
Abbas Attaf <abbas.attaf@gmail.com>
2019-04-04 13:43:20 +0200

    Création des DTOs et Transformers pour les caractéristiques

--- src/main/java/com/projet/ecommerce/business/dto/CaracteristiqueDTO.java
+++ src/main/java/com/projet/ecommerce/business/dto/CaracteristiqueDTO.java
@@ -2,6 +2,43 @@ package com.projet.ecommerce.business.dto;
 
 public class CaracteristiqueDTO {
 
-    
+    private int idCaracteristique;
 
+    private TypeCaracteristiqueDTO typeCaracteristique;
+
+    private String valeur;
+
+    private ProduitDTO produit;
+
+    public int getIdCaracteristique() {
+        return idCaracteristique;
+    }
+
+    public void setIdCaracteristique(int idCaracteristique) {
+        this.idCaracteristique = idCaracteristique;
+    }
+
+    public TypeCaracteristiqueDTO getTypeCaracteristique() {
+        return typeCaracteristique;
+    }
+
+    public void setTypeCaracteristique(TypeCaracteristiqueDTO typeCaracteristique) {
+        this.typeCaracteristique = typeCaracteristique;
+    }
+
+    public String getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(String valeur) {
+        this.valeur = valeur;
+    }
+
+    public ProduitDTO getProduit() {
+        return produit;
+    }
+
+    public void setProduit(ProduitDTO produit) {
+        this.produit = produit;
+    }
 }
---
+++ src/main/java/com/projet/ecommerce/business/dto/TypeCaracteristiqueDTO.java
@@ -0,0 +1,24 @@
+package com.projet.ecommerce.business.dto;
+
+public class TypeCaracteristiqueDTO {
+
+    private int idTypeCaracteristique;
+
+    private String libelle;
+
+    public int getIdTypeCaracteristique() {
+        return idTypeCaracteristique;
+    }
+
+    public void setIdTypeCaracteristique(int idTypeCaracteristique) {
+        this.idTypeCaracteristique = idTypeCaracteristique;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+}
