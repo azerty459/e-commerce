6c57f262b84d7b2362369b6ced73220ec9dc02c4
Arthur Brandao <abrandao@nextoo.fr>
2019-04-05 17:19:39 +0200

    :sparkles: Ajout service Caracteristique

---
+++ src/main/java/com/projet/ecommerce/business/impl/CaracteristiqueBusiness.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projet.ecommerce.business.impl;
+
+import com.projet.ecommerce.business.ICaracteristiqueBusiness;
+import com.projet.ecommerce.business.dto.CaracteristiqueDto;
+import com.projet.ecommerce.business.dto.transformer.CaracteristiqueTransformer;
+import com.projet.ecommerce.entrypoint.graphql.GraphQLCustomException;
+import com.projet.ecommerce.persistance.entity.Caracteristique;
+import com.projet.ecommerce.persistance.repository.CaracteristiqueRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.projet.ecommerce.utilitaire.Utilitaire.mergeObjects;
+import java.util.Collection;
+
+
+public class CaracteristiqueBusiness implements ICaracteristiqueBusiness {
+    
+    @Autowired
+    private CaracteristiqueRepository caracteristiqueRepository;
+
+    @Override
+    public CaracteristiqueDto add(String libelle) {
+        if(libelle == null || libelle.trim().isEmpty()) {
+            GraphQLCustomException gqlex = new GraphQLCustomException("Erreur dans l'ajout de la caracteristique (le libellé ne peut pas être null)");
+            gqlex.ajouterExtension("libelle", libelle);
+            throw gqlex;
+        }
+        Caracteristique c = new Caracteristique();
+        c.setLibelle(libelle);
+        return CaracteristiqueTransformer.entityToDto(caracteristiqueRepository.save(c));
+    }
+
+    @Override
+    public CaracteristiqueDto update(CaracteristiqueDto carac) {
+        if(carac == null) {
+            return null;
+        } else if(carac.getLibelle().trim().isEmpty()) {
+            GraphQLCustomException gqlex = new GraphQLCustomException("Erreur dans l'ajout de la caracteristique (le libellé ne peut pas être null)");
+            gqlex.ajouterExtension("libelle", carac.getLibelle());
+            throw gqlex;
+        }
+        //Recherche dans la bse si il existe deja
+        Optional<Caracteristique> optCarac = caracteristiqueRepository.findById(carac.getId());
+        optCarac.orElseThrow(() -> new GraphQLCustomException("La caractéristique recherchée n'existe pas"));
+        Caracteristique caracteristiqueBase = optCarac.get();
+        Caracteristique caracteristique = CaracteristiqueTransformer.dtoToEntity(carac);
+        
+        try {
+            Caracteristique caracteristique1Result = mergeObjects(caracteristiqueBase, caracteristique);
+            return CaracteristiqueTransformer.entityToDto(caracteristiqueRepository.save(caracteristique1Result));
+        } catch (IllegalAccessException | InstantiationException ex) {
+            throw new GraphQLCustomException("Le type de caractéristique ne peut pas être sauvegardé");
+        }
+    }
+
+    @Override
+    public boolean delete(int id) {
+        Optional<Caracteristique> optCarac = caracteristiqueRepository.findById(id);
+        if(!optCarac.isPresent()) {
+            return false;
+        }
+        caracteristiqueRepository.deleteById(id);
+        return true;
+
+    }
+
+    @Override
+    public Collection<CaracteristiqueDto> getAll() {
+        Iterable<Caracteristique> collectionCarac = caracteristiqueRepository.findAll();
+        return CaracteristiqueTransformer.entityToDto(collectionCarac);
+    }
+    
+}
