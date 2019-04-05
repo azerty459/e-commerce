780d34c6e06f700ffb43454571678ead07fc84dc
julien <jdutrieux@nextoo.fr>
2019-04-05 10:25:40 +0200

    ajout transformer de collection pour les types de caractéristique

--- src/main/java/com/projet/ecommerce/business/dto/transformer/TypeCaracteristiqueTransformer.java
+++ src/main/java/com/projet/ecommerce/business/dto/transformer/TypeCaracteristiqueTransformer.java
@@ -1,6 +1,12 @@
 package com.projet.ecommerce.business.dto.transformer;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.projet.ecommerce.business.dto.CaracteristiqueDTO;
 import com.projet.ecommerce.business.dto.TypeCaracteristiqueDTO;
+import com.projet.ecommerce.persistance.entity.Caracteristique;
 import com.projet.ecommerce.persistance.entity.TypeCaracteristique;
 
 public class TypeCaracteristiqueTransformer {
@@ -19,6 +25,17 @@ public class TypeCaracteristiqueTransformer {
         
     }
     
+    public static Collection<TypeCaracteristiqueDTO> entityToDto(Collection<TypeCaracteristique> TypeCaracteristiques) {
+        if (TypeCaracteristiques == null) {
+            return null;
+        }
+        List<TypeCaracteristiqueDTO> typeCaracteristiqueDTOCollection = new ArrayList<>();
+        for (TypeCaracteristique typeCaracteristique : TypeCaracteristiques) {
+        	typeCaracteristiqueDTOCollection.add(entityToDto(typeCaracteristique));
+        }
+        return typeCaracteristiqueDTOCollection;
+    }
+    
     /**
      * Transforme un objet TypeCaracteristique en un objet TypeCaracteristiqueDTO.
      *
@@ -31,6 +48,17 @@ public class TypeCaracteristiqueTransformer {
 		typeCaracteristiqueDto.setNomType(typeCaracteristique.getNomTypeCaracteristique());
         return typeCaracteristiqueDto;
         
+    }
+    
+    public static Collection<TypeCaracteristique> dtoToEntity(Collection<TypeCaracteristiqueDTO> typeCaracteristiquesDto) {
+        if (typeCaracteristiquesDto == null) {
+            return null;
+        }
+        List<TypeCaracteristique> typeCaracteristiqueCollection = new ArrayList<>();
+        for (TypeCaracteristiqueDTO typeCaracteristiqueDto : typeCaracteristiquesDto) {
+        	typeCaracteristiqueCollection.add(dtoToEntity(typeCaracteristiqueDto));
+        }
+        return typeCaracteristiqueCollection;
     }
 
 }
