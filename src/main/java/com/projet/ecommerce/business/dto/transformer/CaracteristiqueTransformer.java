d71b1a847298f9f177fe8a6bcc180af1857ab1a7
Arthur Brandao <abrandao@nextoo.fr>
2019-04-05 17:21:46 +0200

    :art: Retrait lignes inutiles

--- src/main/java/com/projet/ecommerce/business/dto/transformer/CaracteristiqueTransformer.java
+++ src/main/java/com/projet/ecommerce/business/dto/transformer/CaracteristiqueTransformer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.projet.ecommerce.business.dto.transformer;
 
 import com.projet.ecommerce.business.dto.CaracteristiqueDto;
@@ -10,10 +5,6 @@ import com.projet.ecommerce.persistance.entity.Caracteristique;
 import java.util.ArrayList;
 import java.util.Collection;
 
-/**
- *
- * @author loquicom
- */
 public class CaracteristiqueTransformer {
     
     public static CaracteristiqueDto entityToDto(Caracteristique carac) {
