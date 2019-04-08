179ad2a6dfb0b72a90d35d85ce5c0bda7a3f4060
NicolasTheRookie <n.kowalski@comptoirducode.com>
2019-04-08 11:37:57 +0200

    Ajout méthode findallWithCriteriaRequeteComplexe dans l interface

--- src/main/java/com/projet/ecommerce/persistance/repository/ProduitRepositoryCustom.java
+++ src/main/java/com/projet/ecommerce/persistance/repository/ProduitRepositoryCustom.java
@@ -1,11 +1,14 @@
 package com.projet.ecommerce.persistance.repository;
 
-import com.projet.ecommerce.persistance.entity.Produit;
-
 import java.util.Collection;
+
+import com.projet.ecommerce.persistance.entity.Categorie;
+import com.projet.ecommerce.persistance.entity.Produit;
 
 public interface ProduitRepositoryCustom {
 
     Collection<Produit> findAllWithCriteria(String ref, String cat);
+    
+    Collection<Produit> findAllWithCriteriaRequeteComplexe(String nom, String partieNom, double moyenneAvisInferieurA, double moyenneAvisSuperieurA, Categorie categorie);
 
 }
