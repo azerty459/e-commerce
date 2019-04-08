3b4210ad632aa7b08df8037bca7cd1f557ea8414
Olivier DELBOUVE <odelbouve@nextoo.fr>
2019-04-08 14:42:11 +0200

    Correction du merge

--- src/main/java/com/projet/ecommerce/persistance/repository/ProduitRepositoryCustom.java
+++ src/main/java/com/projet/ecommerce/persistance/repository/ProduitRepositoryCustom.java
@@ -1,14 +1,13 @@
 package com.projet.ecommerce.persistance.repository;
 
-import java.util.Collection;
-
-import com.projet.ecommerce.persistance.entity.Categorie;
 import com.projet.ecommerce.persistance.entity.Produit;
+
+import java.util.Collection;
 
 public interface ProduitRepositoryCustom {
 
     Collection<Produit> findAllWithCriteria(String ref, String cat);
-    
-    Collection<Produit> findAllWithCriteriaRequeteComplexe(String nom, String partieNom, double moyenneAvisInferieurA, double moyenneAvisSuperieurA, Categorie categorie);
+
+//    Collection<Produit> findAllWithCriteriaRequeteComplexe(String nom, String partieNom, double moyenneAvisInferieurA, double moyenneAvisSuperieurA, Categorie categorie);
 
 }
