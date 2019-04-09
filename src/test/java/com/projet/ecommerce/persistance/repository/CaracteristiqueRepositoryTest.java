08183632400cfb1d8a4125817054a644b8e4aa79
Arthur Brandao <abrandao@nextoo.fr>
2019-04-09 09:55:17 +0200

    :white_check_mark: Ajout test caracteristique repository

--- src/test/java/com/projet/ecommerce/persistance/repository/CaracteristiqueRepositoryTest.java
+++ src/test/java/com/projet/ecommerce/persistance/repository/CaracteristiqueRepositoryTest.java
@@ -7,6 +7,7 @@ package com.projet.ecommerce.persistance.repository;
 
 import com.projet.ecommerce.persistance.entity.Caracteristique;
 import java.util.Collection;
+import java.util.Optional;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,29 +33,18 @@ public class CaracteristiqueRepositoryTest {
         //Permet d'écraser la config application.properties par application-test.properties
         System.setProperty("spring.config.location", "classpath:application-test.properties");
         
-        TEMP_INSERT.setIdCaracteristique(2);
+        TEMP_INSERT.setIdCaracteristique(8);
         TEMP_INSERT.setLibelle("Luminosité");
         
-        TEMP_GET.setIdCaracteristique(8);
+        TEMP_UPDATE.setIdCaracteristique(16);
+        TEMP_UPDATE.setLibelle("Forme");
+        
+        TEMP_DELETE.setIdCaracteristique(32);
+        TEMP_DELETE.setLibelle("Puissance");
+        
+        TEMP_GET.setIdCaracteristique(64);
         TEMP_GET.setLibelle("Odeur");
 
-        /*
-        TEMP_INSERT.setReferenceProduit("A05A87");
-        TEMP_INSERT.setPrixHT(8.7f);
-        TEMP_INSERT.setDescription("joli produit");
-
-        TEMP_DELETE.setReferenceProduit("A05A88");
-        TEMP_DELETE.setPrixHT(11.7f);
-        TEMP_DELETE.setDescription("produit");
-
-        TEMP_UPDATE.setReferenceProduit("A05A89");
-        TEMP_UPDATE.setPrixHT(10.875f);
-        TEMP_UPDATE.setDescription("joli truc");
-
-        TEMP_GET.setReferenceProduit("A05A90");
-        TEMP_GET.setPrixHT(9.214f);
-        TEMP_GET.setDescription("bas de gamme");
-        */
     }
     
     @Autowired
@@ -67,22 +57,75 @@ public class CaracteristiqueRepositoryTest {
     
     @Test
     public void testInsert() {
+        Caracteristique insert = caracteristiqueRepository.save(TEMP_INSERT);
+        Assert.assertNotNull(insert);
         
+        Optional<Caracteristique> optionalCaracteristique = caracteristiqueRepository.findById(insert.getIdCaracteristique());
+        Assert.assertTrue(optionalCaracteristique.isPresent());
+        Assert.assertEquals(insert.getIdCaracteristique(), optionalCaracteristique.get().getIdCaracteristique());
+        Assert.assertEquals(insert.getLibelle(), optionalCaracteristique.get().getLibelle());
     }
     
     @Test
     public void testUpdate() {
+        Caracteristique insert = caracteristiqueRepository.save(TEMP_INSERT);
         
+        TEMP_UPDATE.setIdCaracteristique(insert.getIdCaracteristique());
+        Caracteristique update = caracteristiqueRepository.save(TEMP_UPDATE);
+        Assert.assertNotNull(update);
+        Assert.assertEquals(TEMP_UPDATE.getIdCaracteristique(), TEMP_UPDATE.getIdCaracteristique());
+        Assert.assertEquals(TEMP_UPDATE.getLibelle(), update.getLibelle());
+        
+        Optional<Caracteristique> optionalCaracteristique = caracteristiqueRepository.findById(update.getIdCaracteristique());
+        Assert.assertTrue(optionalCaracteristique.isPresent());
+        Assert.assertEquals(update.getIdCaracteristique(), optionalCaracteristique.get().getIdCaracteristique());
+        Assert.assertEquals(update.getLibelle(), optionalCaracteristique.get().getLibelle());
     }
     
     @Test
     public void testDelete() {
+        Caracteristique insert = caracteristiqueRepository.save(TEMP_DELETE);
+        caracteristiqueRepository.delete(insert);
+        Assert.assertFalse(caracteristiqueRepository.findById(insert.getIdCaracteristique()).isPresent());
         
+        insert = caracteristiqueRepository.save(TEMP_DELETE);
+        caracteristiqueRepository.deleteById(insert.getIdCaracteristique());
+        Assert.assertFalse(caracteristiqueRepository.findById(insert.getIdCaracteristique()).isPresent());
     }
     
     @Test
     public void testFindById() {
+        Caracteristique insert = caracteristiqueRepository.save(TEMP_GET);
         
+        Optional<Caracteristique> optionalCaracteristique = caracteristiqueRepository.findById(insert.getIdCaracteristique());
+        Assert.assertTrue(optionalCaracteristique.isPresent());
+        Caracteristique caracteristique = optionalCaracteristique.get();
+        Assert.assertEquals(insert.getIdCaracteristique(), caracteristique.getIdCaracteristique());
+        Assert.assertEquals(insert.getLibelle(), caracteristique.getLibelle());
+    }
+    
+    @Test
+    public void testFindAll() {
+        Collection<Caracteristique> all = caracteristiqueRepository.findAll();
+        Assert.assertEquals(0, all.size());
+        
+        Caracteristique caracteristique1 = caracteristiqueRepository.save(TEMP_GET);
+        Caracteristique caracteristique2 = caracteristiqueRepository.save(TEMP_INSERT);
+        all = caracteristiqueRepository.findAll();
+        Assert.assertEquals(2, all.size());
+        for(Caracteristique c : all) {
+            if(c.getIdCaracteristique() != caracteristique1.getIdCaracteristique() && c.getIdCaracteristique() != caracteristique2.getIdCaracteristique()) {
+                Assert.fail("La caracteristique ne correspond pas à une ajoutée");
+            }
+        }
+        
+        caracteristiqueRepository.delete(caracteristique1);
+        all = caracteristiqueRepository.findAll();
+        Assert.assertEquals(1, all.size());
+        for(Caracteristique c : all) {
+            Assert.assertEquals(caracteristique2.getIdCaracteristique(), c.getIdCaracteristique());
+            Assert.assertEquals(caracteristique2.getLibelle(), c.getLibelle());
+        }
     }
     
     @Test
