35786797cba4a59bdf79c62c093a8a3f21592267
Olivier DELBOUVE <odelbouve@nextoo.fr>
2019-04-11 12:07:03 +0200

    Préparation de la branche pour le debug

--- src/test/java/com/projet/ecommerce/business/impl/PhotoBusinessTests.java
+++ src/test/java/com/projet/ecommerce/business/impl/PhotoBusinessTests.java
@@ -2,7 +2,6 @@ package com.projet.ecommerce.business.impl;
 
 import com.projet.ecommerce.persistance.entity.Photo;
 import com.projet.ecommerce.persistance.entity.Produit;
-import com.projet.ecommerce.persistance.repository.PhotoRepository;
 import com.projet.ecommerce.persistance.repository.ProduitRepository;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.junit.Assert;
@@ -10,18 +9,18 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -32,18 +31,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RunWith(MockitoJUnitRunner.Silent.class)
-@SpringBootTest
+@ActiveProfiles(profiles = "test")
 public class PhotoBusinessTests {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PhotoBusinessTests.class);
+    private static final String IMG_ROOT = "src/main/resources/img/";
 
     @Mock
     private ProduitRepository produitRepository;
-
-    @Mock
-    private PhotoRepository photoRepository;
-
-    @Mock
-    private MultipartFile multipartFile;
 
     @InjectMocks
     private PhotoBusiness photoBusiness;
@@ -51,6 +46,7 @@ public class PhotoBusinessTests {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        ReflectionTestUtils.setField(photoBusiness, "repertoireImg", IMG_ROOT);
     }
 
     @Rule
@@ -71,15 +67,15 @@ public class PhotoBusinessTests {
         try {
             content = Files.readAllBytes(path);
         } catch (IOException e) {
-
+            LOGGER.error("testUploadFichierVide", e);
         }
         String contentType = "image/jpeg";
         MultipartFile mpf = new MockMultipartFile(name, originalFileName, contentType, content);
-
         try {
             Assert.assertFalse(photoBusiness.upload(mpf, "hhh"));
         } catch (Exception e) {
-
+            LOGGER.error("testUploadFichierVide", e);
+            Assert.fail();
         }
     }
 
@@ -88,7 +84,6 @@ public class PhotoBusinessTests {
      */
     @Test
     public void testUploadNormal() {
-
         // Construction du fichier
         String name = "file.jpeg";
         String originalFileName = "file.jpeg";
@@ -99,11 +94,11 @@ public class PhotoBusinessTests {
         // Test du 1ier if
         try {
             Assert.assertTrue(photoBusiness.upload(mpf, "hello"));
-        } catch (Exception e) {
-
+        } catch (PhotoException e) {
+            LOGGER.error("testUploadNormal", e);
+            Assert.fail();
         }
     }
-
 
     /**
      * Cas où le fichier n'a pas d'extension
@@ -120,7 +115,7 @@ public class PhotoBusinessTests {
         p.setDateAjout(date);
         p.setPrixHT(44);
         p.setNom("NOM PRODUIT");
-        List<Photo> arrayPhotos = new ArrayList<Photo>();
+        List<Photo> arrayPhotos = new ArrayList<>();
         p.setPhotos(arrayPhotos);
 
 
@@ -149,14 +144,14 @@ public class PhotoBusinessTests {
         p.setDateAjout(date);
         p.setPrixHT(44);
         p.setNom("NOM PRODUIT");
-        List<Photo> arrayPhotos = new ArrayList<Photo>();
+        List<Photo> arrayPhotos = new ArrayList<>();
         p.setPhotos(arrayPhotos);
 
         // Construction du fichier
         String name = "file.jpg";
-        String originalFileName = "file.jpg";
-        byte[] content = "Juste des données au pif".getBytes();
+        String originalFileName = "fileOriginal.jpg";
         String contentType = "image/jpeg";
+        byte[] content = "Juste des données au pif".getBytes();
         MultipartFile mpf = new MockMultipartFile(name, originalFileName, contentType, content);
 
         Mockito.when(produitRepository.findById(Mockito.any())).thenReturn(Optional.of(p));
@@ -164,7 +159,8 @@ public class PhotoBusinessTests {
         try {
             photoBusiness.upload(mpf, p.getReferenceProduit());
         } catch (PhotoException e) {
-
+            LOGGER.error("constructionNouveauRepertoireDeStockage", e);
+            Assert.fail();
         }
 
         try {
@@ -172,20 +168,14 @@ public class PhotoBusinessTests {
             Assert.assertTrue(Files.isDirectory(Paths.get("src/main/resources/img/1900/1/1/REF")));
 
             // Vérifier que le fichier a bien été ajouté et qu'il a le mD5 du fichier comme nom
-            StringBuilder sb = new StringBuilder();
-            DigestUtils.appendMd5DigestAsHex(content, sb);
-            Assert.assertTrue(Files.isRegularFile(Paths.get("src/main/resources/img/1900/1/1/REF/" + sb.toString() + ".jpg")));
+            StringBuilder sbMd5 = new StringBuilder();
+            DigestUtils.appendMd5DigestAsHex(content, sbMd5);
+            Assert.assertTrue(Files.isRegularFile(Paths.get("src/main/resources/img/1900/1/1/REF/" + sbMd5.toString() + ".jpg")));
         } catch (Exception e) {
 
         } finally {
             // Suppression du fichier et dossier créé
-            try {
-
-                FileUtils.deleteDirectory(new File("src/main/resources/img/1900/"));
-                System.out.println("Le dossier créé durant le test a été supprimé.");
-            } catch (IOException e) {
-                System.out.println("Le dossier créé durant le test  n'a pas été supprimé.");
-            }
+            forceDeleteProductDirectory(Paths.get("src/main/resources/img/1900/1/1/REF"));
         }
     }
 
@@ -205,7 +195,7 @@ public class PhotoBusinessTests {
         p.setDateAjout(date);
         p.setPrixHT(44);
         p.setNom("NOM PRODUIT");
-        List<Photo> arrayPhotos = new ArrayList<Photo>();
+        List<Photo> arrayPhotos = new ArrayList<>();
         p.setPhotos(arrayPhotos);
 
         // Construction du fichier
@@ -219,26 +209,34 @@ public class PhotoBusinessTests {
 
         // 1ier ajout
         try {
-            System.out.println("1ier ajout");
+            LOGGER.info("1ier ajout");
             photoBusiness.upload(mpf, p.getReferenceProduit());
         } catch (PhotoException e) {
-
+            // Suppression du fichier et dossier créé
+            forceDeleteProductDirectory(Paths.get("src/main/resources/img/1900/1/1/REF"));
+            LOGGER.error("constructionNouveauRepertoireDeStockage", e);
+            Assert.fail();
         }
 
         // 2eme ajout
         try {
-            System.out.println("2e ajout");
+            LOGGER.info("2e ajout");
             photoBusiness.upload(mpf, p.getReferenceProduit());
         } catch (PhotoException e) {
             throw new PhotoException("Test OK");
         } finally {
             // Suppression du fichier et dossier créé
-            try {
-                FileUtils.deleteDirectory(new File("src/main/resources/img/1900/"));
-                System.out.println("Le dossier créé durant le test a été supprimé.");
-            } catch (IOException e) {
-                System.out.println("Le dossier créé durant le test  n'a pas été supprimé.");
-            }
+            forceDeleteProductDirectory(Paths.get("src/main/resources/img/1900/1/1/REF"));
+        }
+    }
+
+    private void forceDeleteProductDirectory(Path productPath) {
+        try {
+            FileUtils.forceDeleteOnExit(productPath.toFile());
+            LOGGER.info("Le dossier créé durant le test a été supprimé.");
+        } catch (IOException e) {
+            LOGGER.error("Erreur lors de la suppression du dossier de test d'image de produit!", e);
+            Assert.fail();
         }
     }
 }
