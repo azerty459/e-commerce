538feb5de1220f28ebff07a641e3ceb175a53c94
Arthur Brandao <abrandao@nextoo.fr>
2019-05-06 14:33:15 +0200

    Ajout import logger

--- src/main/java/com/projet/ecommerce/business/impl/PhotoBusiness.java
+++ src/main/java/com/projet/ecommerce/business/impl/PhotoBusiness.java
@@ -11,6 +11,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
