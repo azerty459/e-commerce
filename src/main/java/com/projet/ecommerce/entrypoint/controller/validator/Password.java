e3d93450f09a8fca2b339ee4c88e2fb426c4438c
Arthur Brandao <abrandao@nextoo.fr>
2019-05-27 15:28:58 +0200

    Creation validateur de mot de passe

---
+++ src/main/java/com/projet/ecommerce/entrypoint/controller/validator/Password.java
@@ -0,0 +1,19 @@
+package com.projet.ecommerce.entrypoint.controller.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Password {
+
+	String message() default "Password";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+}
