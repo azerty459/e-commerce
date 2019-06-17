3c156f39cb39204cd0515d14f68801e0a7fc2317
ludovic lahousse <llahousse@nextoo.fr>
2019-06-17 15:13:48 +0200

    Mise en place de tout les tests pour le controller Categorie

--- src/test/java/com/projet/ecommerce/entrypoint/controller/ControllerCategorieTests.java
+++ src/test/java/com/projet/ecommerce/entrypoint/controller/ControllerCategorieTests.java
@@ -26,7 +26,7 @@ public class ControllerCategorieTests {
 
 
 	@Mock
-	private CategorieBusiness categorieBusiness2;
+	private CategorieBusiness categorieBusiness;
 
 	@Mock
 	private CategorieSupprimeBusiness categorieSupprimeBusiness;
@@ -66,22 +66,139 @@ public class ControllerCategorieTests {
 
 		Assert.assertEquals(categorieDTOList.size(), 2);
 
-		when(categorieBusiness2.getCategorie(Mockito.anyInt(), Mockito.any(), Mockito.anyBoolean(), Mockito.anyBoolean())).thenReturn(categorieDTOList);
+		when(categorieBusiness.getCategorie(Mockito.anyInt(), Mockito.any(), Mockito.anyBoolean(), Mockito.anyBoolean())).thenReturn(categorieDTOList);
 
 		List<CategorieDTO> listRetour = new ArrayList<>(controllerCategorie.getAllCategorie());
 
-		Assert.assertEquals(categorieDTOList.size(), listRetour.size());
+		Assert.assertEquals(listRetour.size(), categorieDTOList.size());
+		Assert.assertEquals(listRetour.get(0).getId(), categorieDTO_1.getId());
+		Assert.assertEquals(listRetour.get(0).getNom(), categorieDTO_1.getNom());
+		Assert.assertEquals(listRetour.get(0).getLevel(), categorieDTO_1.getLevel());
+		Assert.assertEquals(listRetour.get(0).getProfondeur(), categorieDTO_1.getProfondeur());
 
+		Assert.assertEquals(listRetour.get(1).getId(), categorieDTO_2.getId());
+		Assert.assertEquals(listRetour.get(1).getNom(), categorieDTO_2.getNom());
+		Assert.assertEquals(listRetour.get(1).getLevel(), categorieDTO_2.getLevel());
+		Assert.assertEquals(listRetour.get(1).getProfondeur(), categorieDTO_2.getProfondeur());
+	}
 
-//		Assert.assertEquals(listRetour.get(0).getId(), categorieDTO_1.getId());
-//		Assert.assertEquals(listRetour.get(0).getNom(), categorieDTO_1.getNom());
-//		Assert.assertEquals(listRetour.get(0).getLevel(), categorieDTO_1.getLevel());
-//		Assert.assertEquals(listRetour.get(0).getProfondeur(), categorieDTO_1.getProfondeur());
-//
-//		Assert.assertEquals(listRetour.get(1).getId(), categorieDTO_2.getId());
-//		Assert.assertEquals(listRetour.get(1).getNom(), categorieDTO_2.getNom());
-//		Assert.assertEquals(listRetour.get(1).getLevel(), categorieDTO_2.getLevel());
-//		Assert.assertEquals(listRetour.get(1).getProfondeur(), categorieDTO_2.getProfondeur());
+	@Test
+	public void getCategorieById() {
+		CategorieDTO categorieDTO = buildCategorieDTO(3, "dessin", 2, 4);
+		List<CategorieDTO> categorieDTOList = new ArrayList<>();
+		categorieDTOList.add(categorieDTO);
+
+		when(categorieBusiness.getCategorie(Mockito.anyInt(), Mockito.any(), Mockito.anyBoolean(), Mockito.anyBoolean())).thenReturn(categorieDTOList);
+
+		List<CategorieDTO> listRetour = new ArrayList<>(controllerCategorie.getCategorieById(categorieDTO.getId()));
+		Assert.assertEquals(listRetour.size(), categorieDTOList.size());
+		Assert.assertEquals(listRetour.get(0).getId(), categorieDTO.getId());
+		Assert.assertEquals(listRetour.get(0).getNom(), categorieDTO.getNom());
+		Assert.assertEquals(listRetour.get(0).getLevel(), categorieDTO.getLevel());
+		Assert.assertEquals(listRetour.get(0).getProfondeur(), categorieDTO.getProfondeur());
+	}
+
+	@Test
+	public void getSubCategory() {
+		CategorieDTO categorieDTO = buildCategorieDTO(3, "dessin", 3, 4);
+		categorieDTO.getSousCategories().add(buildCategorieDTO(4, "photoshop", 1, 2));
+
+		List<CategorieDTO> categorieDTOList = new ArrayList<>();
+		categorieDTOList.add(categorieDTO);
+
+		when(categorieBusiness.getCategorie(Mockito.anyInt(), Mockito.any(), Mockito.anyBoolean(), Mockito.anyBoolean())).thenReturn(categorieDTO.getSousCategories());
+
+		List<CategorieDTO> listRetour = new ArrayList<>(controllerCategorie.getSubCategory(categorieDTO.getNom()));
+		Assert.assertEquals(listRetour.size(), categorieDTOList.size());
+		Assert.assertEquals(listRetour.get(0).getId(), categorieDTO.getSousCategories().get(0).getId());
+		Assert.assertEquals(listRetour.get(0).getNom(), categorieDTO.getSousCategories().get(0).getNom());
+		Assert.assertEquals(listRetour.get(0).getLevel(), categorieDTO.getSousCategories().get(0).getLevel());
+		Assert.assertEquals(listRetour.get(0).getProfondeur(), categorieDTO.getSousCategories().get(0).getProfondeur());
+	}
+
+	@Test
+	public void deleteCategoryById() {
+		CategorieDTO categorieDTO = buildCategorieDTO(5, "gimp", 3, 4);
+		when(categorieBusiness.delete(Mockito.anyInt())).thenReturn(Boolean.TRUE);
+
+		Boolean bool = controllerCategorie.deleteCategorieById(categorieDTO.getId());
+
+		Assert.assertTrue(bool);
+	}
+
+	@Test
+	public void updateCategorieDTO() {
+		CategorieDTO categorieDTO = buildCategorieDTO(6, "nimp", 5, 6);
+
+		when(categorieBusiness.updateCategorie(Mockito.anyInt(), Mockito.anyString())).thenReturn(categorieDTO);
+
+		CategorieDTO retour = controllerCategorie.updateCategorieDTO(categorieDTO.getId(), categorieDTO.getNom());
+
+		Assert.assertNotNull(retour);
+		Assert.assertEquals(retour.getId(), categorieDTO.getId());
+		Assert.assertEquals(retour.getNom(), categorieDTO.getNom());
+		Assert.assertEquals(retour.getLevel(), categorieDTO.getLevel());
+		Assert.assertEquals(retour.getProfondeur(), categorieDTO.getProfondeur());
+	}
+
+	@Test
+	public void moveCategorie() {
+		CategorieDTO categorieDTO_1 = buildCategorieDTO(7, "photo", 1, 2);
+		CategorieDTO categorieDTO_2 = buildCategorieDTO(8, "gimp", 2, 3);
+
+		when(categorieBusiness.moveCategorie(Mockito.anyInt(), Mockito.anyInt())).thenReturn(Boolean.TRUE);
+
+		Boolean bool = controllerCategorie.moveCategorie(categorieDTO_1.getId(), categorieDTO_2.getId());
+
+		Assert.assertTrue(bool);
+	}
+
+	@Test
+	public void restoreCategorie() {
+		CategorieDTO categorieDTO = buildCategorieDTO(1, "gimp", 1, 2);
+		List<CategorieDTO> categorieDTOList = new ArrayList<>();
+		categorieDTOList.add(categorieDTO);
+
+		when(categorieSupprimeBusiness.restoreLastDeletedCategorie(Mockito.anyInt())).thenReturn(categorieDTOList);
+
+		List<CategorieDTO> listRetour = new ArrayList<>(controllerCategorie.restoreCategorie(categorieDTO.getId()));
+
+		Assert.assertNotNull(categorieDTO);
+		Assert.assertEquals(listRetour.size(), categorieDTOList.size());
+		Assert.assertEquals(listRetour.get(0).getId(), categorieDTOList.get(0).getId());
+		Assert.assertEquals(listRetour.get(0).getNom(), categorieDTOList.get(0).getNom());
+		Assert.assertEquals(listRetour.get(0).getLevel(), categorieDTOList.get(0).getLevel());
+		Assert.assertEquals(listRetour.get(0).getProfondeur(), categorieDTOList.get(0).getProfondeur());
+	}
+
+	@Test
+	public void addCategorieParent() {
+		CategorieDTO categorieDTO = buildCategorieDTO(8, "toto", 2, 4);
+
+		when(categorieBusiness.addParent(Mockito.anyString())).thenReturn(categorieDTO);
+
+		CategorieDTO retour = controllerCategorie.addCategorieParent(categorieDTO.getNom());
+
+		Assert.assertNotNull(retour);
+		Assert.assertEquals(retour.getId(), categorieDTO.getId());
+		Assert.assertEquals(retour.getNom(), categorieDTO.getNom());
+		Assert.assertEquals(retour.getLevel(), categorieDTO.getLevel());
+		Assert.assertEquals(retour.getProfondeur(), categorieDTO.getProfondeur());
+	}
+
+	@Test
+	public void addCategorieEnfant() {
+		CategorieDTO categorieDTO = buildCategorieDTO(18, "tata", 4, 4);
+
+		when(categorieBusiness.addEnfant(Mockito.anyString(), Mockito.anyInt())).thenReturn(categorieDTO);
+
+		CategorieDTO retour = controllerCategorie.addCategorieEnfant(categorieDTO.getNom(), categorieDTO.getId());
+
+		Assert.assertNotNull(retour);
+		Assert.assertEquals(retour.getId(), categorieDTO.getId());
+		Assert.assertEquals(retour.getNom(), categorieDTO.getNom());
+		Assert.assertEquals(retour.getLevel(), categorieDTO.getLevel());
+		Assert.assertEquals(retour.getProfondeur(), categorieDTO.getProfondeur());
 	}
 
 }
