223cbd6c4d46098c4cfb6ee64170e455770c284f
yechchouik <yechchouik@nextoo.fr>
2019-11-19 14:33:27 +0100

    :white_check_mark: Add test for criteria

--- src/test/java/com/projet/ecommerce/persistance/repository/ProduitRepositoryCustomTests.java
+++ src/test/java/com/projet/ecommerce/persistance/repository/ProduitRepositoryCustomTests.java
@@ -1,7 +1,10 @@
 package com.projet.ecommerce.persistance.repository;
 
+import com.projet.ecommerce.persistance.entity.AvisClient;
 import com.projet.ecommerce.persistance.entity.Categorie;
+import com.projet.ecommerce.persistance.entity.Photo;
 import com.projet.ecommerce.persistance.entity.Produit;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +17,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,34 +29,20 @@ import java.util.List;
 @Transactional
 public class ProduitRepositoryCustomTests {
 
-    private static final Produit TEMP_INSERT;
-    private static final Categorie TEMP_CATEGORIE;
+    private static Categorie TEMP_CATEGORIE;
+    private static Produit TEMP_INSERT;
 
     static {
         //Permet d'écraser la config application.properties par application-test.properties
         System.setProperty("spring.config.location", "classpath:application-test.properties");
 
-        TEMP_INSERT = new Produit();
-        TEMP_INSERT.setReferenceProduit("A05A87");
-        TEMP_INSERT.setPrixHT(8.7f);
-        TEMP_INSERT.setDescription("joli produit");
-        TEMP_INSERT.setCategories(new ArrayList<>());
-
-        TEMP_CATEGORIE = new Categorie();
-        TEMP_CATEGORIE.setNomCategorie("Livre");
-        TEMP_CATEGORIE.setBorneGauche(1);
-        TEMP_CATEGORIE.setBorneDroit(2);
-        TEMP_CATEGORIE.setLevel(1);
-        TEMP_CATEGORIE.setProduits(new ArrayList<>());
-
-        Collection<Categorie> categorieCollection = TEMP_INSERT.getCategories();
-        categorieCollection.add(TEMP_CATEGORIE);
-        TEMP_INSERT.setCategories(new ArrayList<>(categorieCollection));
     }
 
     @Before
     public void insertProduit() {
-        produitRepository.save(TEMP_INSERT);
+        for (int i=1; i<=4; i++) {
+            produitRepository.save(buildProduit(i));
+        }
     }
 
     @Autowired
@@ -75,6 +68,7 @@ public class ProduitRepositoryCustomTests {
     @Test
     @Transactional
     public void findAllWithCriteriaByCat() {
+        TEMP_CATEGORIE = buildCategorie();
         List<Produit> retourProduitCollection = new ArrayList<>(produitRepository.findAllWithCriteria(null, "Livre"));
         Assert.assertEquals(1, retourProduitCollection.size());
         Assert.assertEquals(retourProduitCollection.get(0).getReferenceProduit(), TEMP_INSERT.getReferenceProduit());
@@ -83,5 +77,71 @@ public class ProduitRepositoryCustomTests {
         Assert.assertEquals(retourProduitCollection.get(0).getCategories().get(0).getNomCategorie(), TEMP_CATEGORIE.getNomCategorie());
 
         Assert.assertEquals(0, produitRepository.findAllWithCriteria("Toto", null).size());
+    }
+
+    @Test
+    public void findAllWithParams() {
+        List<Produit> produitList = produitRepository.findAllWithParams(null, null, 8.0, 9.0, null);
+        //Assert.assertEquals(1, produitList.size());
+        System.out.println("====================================================================================================");
+        System.out.println(produitList.toString());
+        //produitList.stream().forEach(System.out::println);
+        System.out.println("====================================================================================================");
+    }
+
+    //Entity Builders
+    @NotNull
+    private static Produit buildProduit(int numb) {
+        Produit produit = new Produit();
+        //Propduct
+        produit.setReferenceProduit("A05A8" + numb);
+        produit.setDescription(randomAlphabetic(10));
+        produit.setNom("Livre :: " + numb);
+        produit.setPrixHT(numb * 10);
+        //Category list
+        produit.setCategories(new ArrayList<>());
+        //Avis list
+        produit.setAvisClients(new ArrayList<>());
+
+        //Create category list
+        List<Categorie> categorieList = new ArrayList<>();
+        categorieList.add(buildCategorie());
+        produit.setCategories(categorieList);
+        //Create avis list
+        List<AvisClient> avisClientList = new ArrayList<>();
+        for (int i=0; i<2; i++) {
+            avisClientList.add(buildAvisClient(produit));
+        }
+        produit.setAvisClients(avisClientList);
+        return produit;
+    }
+
+    /**
+     * Category builder
+     *
+     * @return
+     */
+    @NotNull
+    private static Categorie buildCategorie() {
+        Categorie categorie = new Categorie();
+        categorie.setNomCategorie("Livre cat");
+        categorie.setBorneGauche(Integer.parseInt(randomNumeric(1)));
+        categorie.setBorneDroit(Integer.parseInt(randomNumeric(1)));
+        categorie.setLevel(Integer.parseInt(randomNumeric(1)));
+        return categorie;
+    }
+
+    /**
+     * Avis client builder
+     *
+     * @return
+     */
+    @NotNull
+    private static AvisClient buildAvisClient(Produit produit) {
+        AvisClient avisClient = new AvisClient();
+        avisClient.setDescription(randomAlphabetic(10));
+        avisClient.setNote(Integer.parseInt(randomNumeric(1)));
+        avisClient.setProduit(produit);
+        return avisClient;
     }
 }
